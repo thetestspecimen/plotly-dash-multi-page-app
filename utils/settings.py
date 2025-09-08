be6e6881f64e0b87c216163d2fa71cc636c6ded6
thetestspecimen <info@thetestspecimen.com>
2025-09-08 21:46:26 +0300

    Fix DEBUG .env variable conversion to bool

--- utils/settings.py
+++ utils/settings.py
@@ -1,8 +1,9 @@
 import os
+from distutils.util import strtobool
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-APP_DEBUG = bool(os.getenv("DEBUG"))
+APP_DEBUG = bool(strtobool(os.getenv("DEBUG", "True")))
 NINJAS_API_KEY = os.getenv("NINJAS_API_KEY")
